d9f89e96416397bef25605699b3378b2204ca586
Qing Bin Liang <leunghb@hotmail.com>
2023-04-10 00:15:07 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+    }
+    get countSides() {
+      return this.sides.length;
+    }
+    get perimeter() {
+      return this.sides.reduce((acc, val) => acc + val, 0);
+    }
+  }
+  
+  class Triangle extends Polygon {
+    get isValid() {
+      const [a, b, c] = this.sides;
+      return a + b > c && a + c > b && b + c > a;
+    }
+  }
+  
+  class Square extends Polygon {
+    get isValid() {
+      return this.sides.every(side => side === this.sides[0]);
+    }
+    get area() {
+      return this.sides[0] ** 2;
+    }
+  }
